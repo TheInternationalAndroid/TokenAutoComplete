0263baad3a82831d0ea85ff41bfdaafddb54a07d
Rayman.t.Yan <tao.yan@istuary.com>
2016-10-11 14:22:29 +0800

    Custom background

--- example/src/main/java/com/tokenautocomplete/TokenTextView.java
+++ example/src/main/java/com/tokenautocomplete/TokenTextView.java
@@ -2,14 +2,11 @@ package com.tokenautocomplete;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * Created by mgod on 5/27/15.
- *
+ * <p>
  * Simple custom view example to show how to get selected events from the token
  * view. See ContactsCompletionView and contact_token.xml for usage
  */
@@ -23,10 +20,9 @@ public class TokenTextView extends TextView {
         super(context, attrs);
     }
 
-
     @Override
     public void setSelected(boolean selected) {
         super.setSelected(selected);
-        setCompoundDrawablesWithIntrinsicBounds(0, 0, selected ? R.drawable.close_x : 0, 0);
+        setCompoundDrawablesWithIntrinsicBounds(0, 0, selected ? 0 : 0, 0);
     }
 }
